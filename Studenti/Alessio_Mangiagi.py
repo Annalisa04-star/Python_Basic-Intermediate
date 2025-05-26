5dc7a45498bce0f63fd7ae137dd21da6b5f60e55
Alessio Mangiagi <alessiomangiagilavoro@gmail.com>
2025-05-26 16:47:05 +0200

    1°

---
+++ Studenti/Alessio_Mangiagi.py
@@ -0,0 +1,34 @@
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+'''print("hello world")
+#funzione salutami
+def salutami(name):
+    x = "hello \n"
+    y = name
+    if name == "peppino":
+        print("Ciao dev, come stai?")
+    elif name == "alessio":
+        print("Ciao Padrone, come stai?")
+    else: 
+        print("Ciao, come stai?")
+    frase= x+y
+    print(frase)
+
+name = input("Inserisci il tuo nome: ")
+
+salutami(name)'''
+
+
+'''def mate():
+    a = int(input("Inserisci il primo numero: "))
+    b = int(input("Inserisci il secondo numero: "))
+    somma = a + b
+    moltiplicazione = a * b
+    dividisione = a / b
+    risultato = f"La somma è: {somma}, la moltiplicazione è: {moltiplicazione}, la divisione è: {dividisione}"
+    logging.info("il risultato è stato registrato")
+    return risultato
+print(mate())
+#si può usare per definire più funzioni e avere il risultato'''
