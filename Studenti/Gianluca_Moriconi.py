1a5c44d279ce1225833d77a5e7a8f268ec711625
gianluca <gianlucamoriconi08@gmail.com>
2025-05-27 16:00:12 +0200

    update 4 - aggiunta calcolatrice

--- Studenti/Gianluca_Moriconi.py
+++ Studenti/Gianluca_Moriconi.py
@@ -8,31 +8,75 @@ def salutami(name): #definisco la funzione salutami, dichiarando la variabile na
 
 #----------------------------------------------------------------------fine funzione--------------------------------------------------------
 
-name = input("inserisci il tuo nome:") #richiede in input un valore string che finisce all'interno 
+name = input("inserisci il tuo nome:")  #richiede in input un valore string che finisce all'interno 
 
-salutami(name) #richiamo la funzione salutami che prende il valore di Nome e lo trasferisce a name
+salutami(name)                          #richiamo la funzione salutami che prende il valore di Nome e lo trasferisce a name
 
-if name == "gian":                 #creo un if con condizione name == gian
-    print("come stai, capo?") # se vero, stampo a schermo questo
-elif name == "giovanni":           #elif è alternativa ad else if. se invece name=giovanni stampo altro
-    print("piacere!")
-else:                              #altrimenti in tutti gli altri casi faccio altro
-    print("piacere di conoscerti, come stai?")
+if name == "gianluca":                  #creo un if con condizione name == gianluca
+    print("come stai, capo?")           # se vero, stampo a schermo questo
+elif name == "giuseppe":                #elif è alternativa ad else if. se invece name=giovanni stampo altro
+    print("piacere rivederti!")
+else:                                   #altrimenti in tutti gli altri casi faccio altro
+    print("piacere di conoscerti!")
 
-#----------------------------------------------------------------------inizio funzione--------------------------------------------------------
+#--------------------------------------------------------------inizio funzione calcolatrice--------------------------------------------------------
 
-def somma():                                         #definisco la funzione somma
-    primnum= input ("inserisci il primo numero: ")   #inserisco in input un numero a primnum
-    secnum= input ("inserisci il secondo numero: ")  #inserisco in input un numero a secnum. tutti i dati presi da input() sono inizialmente di tipo sting..
-                                                     #perciò vanno convertiti in diverso tipo se servono int, float ecc
-    somma= int(primnum) + int(secnum)                #creo la variabile somma che è il risultato della somma di primnum e secnum convertiti in int
-    risultato= f"La somma è: {somma}\n"                #creo la variabile testuale risultato che è una fstring contenente testo + somma
-    return risultato                                 #imposto il return su risultato così da restituire il suo valore quando la funzione termina
-                                                     #il return poi si reimposta in un valore null
+def calcolatrice ():                                    #definisco la funzione calcolatrice
+    print("Benvenuto nella calcolatrice!")              #stampo benvenuto
+    n1 = float(input("inserisci il primo numero: "))    #prendo ad input un valore convertito in float e lo inserisco in n1
+    n2 = float(input("inserisci il secondo numero: "))  #prendo ad input un valore convertito in float e lo inserisco in n2
 
-#----------------------------------------------------------------------fine funzione--------------------------------------------------------
+    operazione = input("inserisci operazione:\n 1. Somma \n 2. Sottrazione \n 3. Moltiplicazione \n 4. Divisione \n") #inserisco una scelta in operazione
+    if operazione == "1":                               #se operazione == 1
+        somma(n1,n2)                                    # faccio somma
+    elif operazione == "2":                             #se operazione == 2
+        sottrazione(n1,n2)                              # faccio somma
+    elif operazione == "3":                             #se operazione == 3
+        moltiplicazione(n1,n2)                          # faccio somma
+    elif operazione == "4":                             #se operazione == 4
+        divisione(n1,n2)                                # faccio somma
+    else:
+        print("azz, c'è un errore")                     #se diverso, do messaggio
 
-print(somma())
+#--------------------------------------------------------------fine funzione calcolatrice--------------------------------------------------------
+
+#----------------------------------------------------------------------inizio funzione somma--------------------------------------------------------
+
+def somma(n1,n2):
+    z = n1 + n2
+    risultato = print (f"Il risultato è: {z}")
+    return risultato                                    #restituisco il valore di risultato
+
+#----------------------------------------------------------------------fine funzione somma--------------------------------------------------------
+
+#----------------------------------------------------------------------inizio funzione sottrazione--------------------------------------------------------
+
+def sottrazione(n1,n2):
+    z = n1 - n2
+    risultato = print (f"Il risultato è: {z}")
+    return risultato
+
+#----------------------------------------------------------------------fine funzione sottrazione--------------------------------------------------------
+
+#----------------------------------------------------------------------inizio funzione moltiplicazione--------------------------------------------------------
+
+def moltiplicazione(n1,n2):
+    z = n1 * n2
+    risultato = print (f"Il risultato è: {z}")
+    return risultato
+
+#----------------------------------------------------------------------fine funzione moltiplicazione--------------------------------------------------------
+
+#----------------------------------------------------------------------inizio funzione divisione--------------------------------------------------------
+
+def divisione(n1,n2):
+    z = n1 / n2
+    risultato = print (f"Il risultato è: {z}")
+    return risultato
+#----------------------------------------------------------------------fine funzione divisione--------------------------------------------------------
+
+calcolatrice() #richiamo la funzione calcolatrice
+
 print(f"questo è un altro modo di scrivere a schermo il risultato: {name}\n")
 totale = name + " " + name
 print(f"questo stampa due stringhe separate da spazi: {totale}\n")
