701c42eac38d23027e06af1b3d835c4b904f6bec
gianluca <gianlucamoriconi08@gmail.com>
2025-05-27 15:22:00 +0200

    updated

--- Studenti/Gianluca_Moriconi.py
+++ Studenti/Gianluca_Moriconi.py
@@ -1,1 +1,39 @@
-print("hello world")
+#----------------------------------------------------------------------inizio funzione--------------------------------------------------------
+
+def salutami(name): #definisco la funzione salutami, dichiarando la variabile name
+    x ="ciao \n"    #definisco x
+    y = name        #definisco y come valore preso da input
+    frase = x + y   #definisco frase come somma dei valori di x e y
+    print(frase)    #stampo frase
+
+#----------------------------------------------------------------------fine funzione--------------------------------------------------------
+
+name = input("inserisci il tuo nome:") #richiede in input un valore string che finisce all'interno 
+
+salutami(name) #richiamo la funzione salutami che prende il valore di Nome e lo trasferisce a name
+
+if name == "gian":                 #creo un if con condizione name == gian
+    print("ciao capo, come stai?") # se vero, stampo a schermo questo
+elif name == "giovanni":           #elif è alternativa ad else if. se invece name=giovanni stampo altro
+    print("piacere giovanni")
+else:                              #altrimenti in tutti gli altri casi faccio altro
+    print("piacere di conoscerti, come stai?")
+
+#----------------------------------------------------------------------inizio funzione--------------------------------------------------------
+
+def somma():                            #definisco la funzione somma
+    primnum= input ("inserisci n1: ")   #inserisco in input un numero a primnum
+    secnum= input ("inserisci 2:")      #inserisco in input un numero a secnum. tutti i dati presi da input() sono inizialmente di tipo sting..
+                                        #perciò vanno convertiti in diverso tipo se servono int, float ecc
+    somma= int(primnum) + int(secnum)   #creo la variabile somma che è il risultato della somma di primnum e secnum convertiti in int
+    risultato= f"La somma è: {somma}"   #creo la variabile testuale risultato che è una fstring contenente testo + somma
+    return risultato                    #imposto il return su risultato così da restituire il suo valore quando la funzione termina
+                                        #il return poi si reimposta in un valore null
+
+#----------------------------------------------------------------------fine funzione--------------------------------------------------------
+
+print(somma())
+print(f"questo è un altro modo di scrivere a schermo il risultato, {name}")
+totale = name + " " + name
+print(f"questo stampa due stringhe separate da spazi: {totale}")
+print(f"questo è un altro modo che stampa due stringhe separate da spazi: {name} {name}")
