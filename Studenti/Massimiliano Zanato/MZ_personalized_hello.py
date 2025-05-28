fdcd6015832126fe68a93fa2e86f2d977c3ccc94
Massimiliano Zanato <lab@massimilianozanato.com>
2025-05-28 02:00:46 +0200

    Correct loop bug into personalized_hello

--- Studenti/Massimiliano Zanato/MZ_personalized_hello.py
+++ Studenti/Massimiliano Zanato/MZ_personalized_hello.py
@@ -4,6 +4,12 @@ def personalized_hello():
      print("---------------------------\nWelcome to the personalized hello application!")
 
      while True:
+        operation = input("---------------------------\nChoose the action:\n1. Create personalized hello\nE. Exit\nSelected operation: ")
+        if operation == "E":
+               break
+        if operation != "1":
+                MZ_utils.print_error("Invalid operation")
+                continue
         name = input("Enter your name: ")
         age = input("Enter your age: ")
 
